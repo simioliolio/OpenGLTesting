6a5a6aa68308b813c7777481faf0a8cb2b924002
simioliolio <simon@oxygn.io>
2016-06-16 18:11:12 +0100

    Initial Commit

---
+++ OpenGLTextureTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLTextureTesting
+//
+//  Created by Simon Haycock on 16/06/2016.
+//  Copyright © 2016 oxygn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLTextureTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLTextureTesting
+//
+//  Created by Simon Haycock on 16/06/2016.
+//  Copyright © 2016 oxygn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
